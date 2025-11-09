713fcb2589b24a00544b09ab8d7d0b075a865015
Eric Chuang <122951029+ericbreh@users.noreply.github.com>
2025-11-08 17:25:31 -0800

    Target pose task (#1)
    * copy template task
    
    * rename
    
    * hardcoded target
    
    * fix initialization
    
    * rename env
    
    * target pose task
    
    * rename experiment

---
+++ source/Dexhand_single/Dexhand_single/tasks/direct/target_pose/__init__.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Target-Pose",
+    entry_point=f"{__name__}.target_pose_env:TargetPoseEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.dexhand_single_env_cfg:DexhandSingleEnvCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
+    },
+)
