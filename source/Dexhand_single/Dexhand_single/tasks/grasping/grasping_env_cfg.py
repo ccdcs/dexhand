620d2c665c41285671c01b29b4c890748eac8828
Eric Chuang <ericchuang94@gmail.com>
2025-11-13 21:14:20 -0800

    add franka but errors

--- source/Dexhand_single/Dexhand_single/tasks/grasping/grasping_env_cfg.py
+++ source/Dexhand_single/Dexhand_single/tasks/grasping/grasping_env_cfg.py
@@ -4,15 +4,19 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 
-from isaaclab.assets import ArticulationCfg
+from isaaclab_assets.robots import FRANKA_PANDA_CFG
+from isaaclab.assets import ArticulationCfg, RigidObjectCfg
 from isaaclab.envs import DirectRLEnvCfg
+
 from isaaclab.scene import InteractiveSceneCfg
 from isaaclab.sim import SimulationCfg
 from isaaclab.utils import configclass
 import isaaclab.sim as sim_utils
 from isaaclab.actuators import ImplicitActuatorCfg
 
-DOFBOT_CONFIG = ArticulationCfg(
+FRANKA_PANDA_CFG.enable_gripper = False
+
+DEXHAND_CFG = ArticulationCfg(
     spawn=sim_utils.UsdFileCfg(
         usd_path="assets/limit_1.57/Assem_DexCo_2/Assem_DexCo_2/Assem_DexCo_2.usd",
         rigid_props=sim_utils.RigidBodyPropertiesCfg(
@@ -34,7 +38,7 @@ DOFBOT_CONFIG = ArticulationCfg(
             "R2_pre_joint": 0.0,
             "R3_pre_joint": 0.0,
         },
-        pos=(0.25, -0.25, 2.0),
+        pos=(0.0, 0.0, 0.0),  # Position will be relative to the arm
     ),
     actuators={
         "L1_act": ImplicitActuatorCfg(
@@ -82,6 +86,10 @@ DOFBOT_CONFIG = ArticulationCfg(
     },
 )
 
+FRANKA_PANDA_CFG.fixed_attachments = {
+    "panda_hand": DEXHAND_CFG,
+}
+
 
 @configclass
 class GraspingEnvCfg(DirectRLEnvCfg):
@@ -89,46 +97,18 @@ class GraspingEnvCfg(DirectRLEnvCfg):
     decimation = 2
     episode_length_s = 5.0
     # - spaces definition
-    action_space = 6
-    observation_space = 12
-    state_space = 6
+    action_space = 13
+    observation_space = 13
+    state_space = 13
 
     # simulation
     sim: SimulationCfg = SimulationCfg(dt=1 / 120, render_interval=decimation)
 
     # robot(s)
-    robot_cfg: ArticulationCfg = DOFBOT_CONFIG.replace(
+    robot_cfg: ArticulationCfg = FRANKA_PANDA_CFG.replace(
         prim_path="/World/envs/env_.*/Robot"
     )
     # scene
     scene: InteractiveSceneCfg = InteractiveSceneCfg(
         num_envs=4096, env_spacing=4.0, replicate_physics=True
     )
-
-    # custom parameters/scales
-    # - controllable joint
-    l1_dof_name = "L1_joint"
-    l2_dof_name = "L2_pre_joint"
-    l3_dof_name = "L3_pre_joint"
-    r1_dof_name = "R1_joint"
-    r2_dof_name = "R2_pre_joint"
-    r3_dof_name = "R3_pre_joint"
-    # - action scale
-    action_scale = 1.0  # [N]
-    # - reward scales
-    rew_scale_alive = 1.0
-    rew_scale_terminated = -2.0
-    rew_scale_pose_error = -1.0
-    # - reset states/conditions
-    target_joint_angle_range = [-1.0, 1.0]
-
-    R = 0.025
-    D1 = 0.018
-    D2 = 0.012
-    D3 = 0.015
-    D4 = 0.010
-    L = 0.045
-    S = 0.006
-
-    action_space_low = [0.012, 0.012, 0.012, 0.012, 0.012, 0.012]
-    action_space_high = [0.027, 0.027, 0.027, 0.027, 0.027, 0.027]
