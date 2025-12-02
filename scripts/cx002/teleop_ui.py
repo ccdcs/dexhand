a0af7d848b3667cff887cdbab6e57f55fcc72389
William Su <wsu@berkeley.edu>
2025-12-01 18:33:22 -0800

    debug

--- scripts/cx002/teleop_ui.py
+++ scripts/cx002/teleop_ui.py
@@ -60,6 +60,16 @@ CX002_CONFIG = ArticulationCfg(
 
 
 class Cx002SceneCfg(InteractiveSceneCfg):
+    ground = AssetBaseCfg(
+        prim_path="/World/defaultGroundPlane",
+        spawn=sim_utils.GroundPlaneCfg()
+    )
+    
+    dome_light = AssetBaseCfg(
+        prim_path="/World/Light",
+        spawn=sim_utils.DomeLightCfg(intensity=2000.0, color=(0.75, 0.75, 0.75)),
+    )
+    
     robot = CX002_CONFIG.replace(prim_path="{ENV_REGEX_NS}/Robot")
 
 
@@ -213,10 +223,6 @@ def main():
 
     scene_cfg = Cx002SceneCfg(num_envs=args_cli.num_envs, env_spacing=2.0)
     scene = InteractiveScene(scene_cfg)
-    
-    spawn_ground_plane(prim_path="/World/ground", cfg=GroundPlaneCfg())
-    light_cfg = sim_utils.DomeLightCfg(intensity=2000.0, color=(0.75, 0.75, 0.75))
-    light_cfg.func("/World/Light", light_cfg)
     
     sim.reset()
     scene.reset()
